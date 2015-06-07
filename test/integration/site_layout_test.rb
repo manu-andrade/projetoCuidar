b66abcb213e46ac051e7e36b22991513e251d94b
Manuela Andrade <manuelandrade_@hotmail.com>
2015-06-07 23:10:06 +0000

    testes criados e funcionando

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,15 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+
  test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", newsletter_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contato_path
     assert_select "a[href=?]", institucional_path
   end
+ end
